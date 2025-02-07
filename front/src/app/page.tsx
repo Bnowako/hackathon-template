5da38685006ac50c97891d9b5346d0821002c16e
Bnowako <blazejnowakowski98@gmail.com>
2025-02-07 15:13:03 +0100

    better form

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,19 +1,39 @@
-import { DummyCard } from "@/components/dummycard";
-import { DummyModel, listDummies } from "@/lib/api/dummyGateway";
-import Image from "next/image";
+"use client"
 
-export default async function Home() {
+import { DummyCard } from "@/components/dummycard";
+import { DummyForm } from "@/components/dummyform";
+import { createDummy, DummyModel, listDummies } from "@/lib/api/dummyGateway";
+import { useEffect, useState } from "react";
+
+export default  function Home() {
+
   
-  const dummies = await listDummies()
-  
+  const [dummies, setdummy] = useState<DummyModel[]>([])
+
+  useEffect(() => {
+    (async () => {
+      setdummy(await listDummies())
+    })()
+  })
+
+  const onSubmit = async (values: any) => {
+    await createDummy(values.name)
+    // # update dummies
+    setdummy(await listDummies())
+  }
+
   return (
     <div>
       <div className="grid grid-cols-1 gap-2 max-w-[50%] mx-auto">
-        
+        <div className="grid grid-cols-2 my-5">
+          <div></div>
+          <DummyForm  onSubmit={onSubmit}/>
+
+        </div>
         {dummies.map((dummy: DummyModel) => (
-          <DummyCard request={dummy} />
+          <DummyCard key={dummy.id} request={dummy} />
         ))}
       </div>
     </div>
-)
+  )
 }
